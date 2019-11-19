1ef57822cc769d3ac9ce26931e4e1f8e83348f39
AmareshSahoo <kumarramaresh@gmail.com>
2019-11-19 22:23:45 +0530

    first commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DattingApp.API.Data;
+using DattingApp.API.Dtos;
+using DattingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DattingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+            if (await _repo.UserExists(userForRegisterDto.Username))
+                return BadRequest("User Already Exist");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUsers = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+    }
+}
---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using DattingApp.API.Models;
+using System.Threading.Tasks;
+
+namespace DattingApp.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExists(string username);
+    }
+}
